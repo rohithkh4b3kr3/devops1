bd624cee9538cee775345c1da2d50cf63e6ad281
rohith0809 <rohithvishva2005@gmail.com>
2025-11-23 15:23:18 +0530

    branch

--- server/app.js
+++ server/app.js
@@ -5,24 +5,25 @@ import cors from "cors";
 const app = express();
 dotenv.config();
 
-app.use(cors(
-    {
-        origin:["http://localhost:5173" ,
-            "http://localhost:3000",
-            "http://localhost:3001",
-            "http://localhost:5174"
-         ],
-    }
-));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:5174",
+    ],
+  }),
+);
 app.use(express.json());
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
 
-app.get("/api/message" , (req,res) => {
-    res.json({message: "message from  server , server is running "})
-    // res.json({message: "server is running"})
+app.get("/api/message", (req, res) => {
+  res.json({ message: "message from  server , server is running " });
+  // res.json({message: "server is running"})
 });
 
-app.listen(PORT , "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
